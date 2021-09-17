724676193b398c84c5dfc20328bc234dfbdf3a4b
William Wang <williambohanwang@gmail.com>
2021-09-17 19:43:16 -0400

    added files relate to dynamic display

--- module_script/plots.js
+++ module_script/plots.js
@@ -1,14 +1,32 @@
-// const url = "https://api.spacexdata.com/v2/launchpads";
+function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+    Plotly.newPlot("plot", data);
+  };
+  
+d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
 
-// d3.json(url).then(receivedData => console.log(receivedData));
-// d3.json(url).then(spaceXResults => console.log(spaceXResults[0].full_name));
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
 
-d3.json("samples.json").then(function(data){
-    console.log(data);
-    wfreq = data.metadata.map(person => person.wfreq).sort((a,b) => b - a);
-    filteredWfreq = wfreq.filter(element => element != null);
-    console.log(filteredWfreq);
+    if (dataset === 'dataset1') {
+        yData = [1, 2, 4, 8, 16];
+    };
 
-    firstPerson = data.metadata[0];
-    Object.entries(firstPerson).forEach(([key, value]) => {console.log(key + ': ' + value);});
-});
+    if (dataset === 'dataset2') {
+        yData = [1, 10, 100, 1000, 10000];
+    };
+
+    var trace = {
+        x: [xData],
+        y: [yData],
+    };
+    
+    Plotly.restyle("plot", trace);
+};
+
+init();
